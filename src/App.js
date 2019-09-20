70c5d23306492a5d2f2c5549d5eec29782fd616d
Natalie-Cardot <nataliecardot@gmail.com>
2019-09-20 14:34:52 -0700

    Add react-bootstrap component instances

--- src/App.js
+++ src/App.js
@@ -8,8 +8,23 @@ let marked = require('marked');
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
+      <h1 class="mt-4">Markdown Previewer</h1>
+      <div class="row mt-5 justify-content-center">
+        <div class="col-xl-11">
+          <FormGroup controlId="formControlsTextarea">
+            {/* Label is for CSS styling, not a Bootstrap class */}
+            <FormLabel class="lead label">Markdown Input</FormLabel>
+            <FormControl componentClass="textarea" placeholder="Enter some markdown." />
+          </FormGroup>
+        </div>
+      </div>
 
+      <div class="row mt-4 justify-content-center">
+        <div class="col-xl-11">
+          <p class="lead label">Markdown Output</p>
+        </div>
+      </div>
     </div>
   );
 }
