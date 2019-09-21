acf087a5173dcc873736fa73b35a3388f52bfab9
Natalie-Cardot <nataliecardot@gmail.com>
2019-09-20 21:05:27 -0700

    Change to arrow function component, add useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import FormGroup from 'react-bootstrap/FormGroup';
 import FormLabel from 'react-bootstrap/FormLabel';
@@ -6,7 +6,12 @@ import FormControl from 'react-bootstrap/FormControl';
 
 let marked = require('marked');
 
-function App() {
+// Since v. 16.8, React supported hooks -- allows you to use state in a function component. Create React App has been using function component instead of class component to help ease the transition https://stackoverflow.com/questions/55889693/why-does-create-react-app-now-use-a-function-component-for-app
+// Changed from function App() { } to arrow function component
+// See https://www.robinwieruch.de/react-function-component
+const App = () => {
+  const [markdown, setMarkdown] = useState('');
+
   return (
     <div className="App container">
       <h1 class="mt-4">Markdown Previewer</h1>
@@ -15,7 +20,7 @@ function App() {
           <FormGroup controlId="formControlsTextarea">
             {/* Label is for CSS styling, not a Bootstrap class */}
             <FormLabel class="lead label">Markdown Input</FormLabel>
-            <FormControl componentClass="textarea" style={{height: 200}} placeholder="Enter some markdown." />
+            <FormControl componentClass="textarea" style={{height: 200}} value={markdown} placeholder="Enter some markdown." />
           </FormGroup>
         </div>
       </div>
@@ -28,6 +33,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
