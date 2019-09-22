959b8f15acacd2e38910a8948249cccdfc51a31f
Natalie-Cardot <nataliecardot@gmail.com>
2019-09-21 20:58:34 -0700

    Adjust styling

--- src/App.js
+++ src/App.js
@@ -20,18 +20,18 @@ const App = () => {
 
   return (
     <div className="App container">
-      <h1 class="mt-4">Markdown Previewer</h1>
+      <h1 class="mt-4 text-center display-4">Markdown Previewer</h1>
       <div class="row mt-5 justify-content-center">
         <div class="col-xl-11">
           <FormGroup controlId="formControlsTextarea">
             {/* Label is for CSS styling, not a Bootstrap class */}
-            <FormLabel class="lead label">Markdown Input</FormLabel>
+            <FormLabel class="lead label mb-4">Markdown Input</FormLabel>
             <FormControl as="textarea" style={{height: 200}} value={markdown} placeholder="Enter some markdown." onChange={event => updateMarkdown(event.target.value)} />
           </FormGroup>
         </div>
       </div>
 
-      <div class="row mt-4 justify-content-center">
+      <div class="row mt-4 justify-content-center mb-4">
         <div class="col-xl-11">
           <p class="lead label">Markdown Output</p>
           <div dangerouslySetInnerHTML={{__html: marked(markdown)}}>
